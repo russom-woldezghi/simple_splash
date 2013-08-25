c88d7b39b8e335f1093f8b8c24b256ad5cc724eb
Russom Woldezghi <russom.woldezghi@gmail.com>
2013-08-25 17:12:37 -0400

    added bg, theming to form, php send function to form

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+// Here we get all the information from the fields sent over by the form.
+$email = $_POST['email'];
+ 
+$to = 'russom@russomwoldezghi.info';
+$subject = 'New subscriber to BMCG';
+$message = 'Email: '.$email .' has subscribed to Bruce Monroe Community Garden using the form on the comming soon page.
+
+Bruce Monroe Community Garden
+www.brucemonroegarden.org
+';
+$headers = 'From: contact@brucemonroegarden.org' . "\r\n";
+ 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
+mail($to, $subject, $message, $headers); //This method sends the mail.
+echo "<div class='success'>Thank you for subscribing.</div>"; // success message
+}else{
+echo "<div class='error'>Invalid Email, please provide an correct email.</div>"; //error message
+}
+
+
+?>
